bfbb4ecfef004c1b2049978381a2600ad797458f
Yemin Sajid <yeminsajid@gmail.com>
2020-08-26 13:44:25 +0600

    Add gridsome

---
+++ src/components/Author.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="author">
+    <div class="square">
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+    </div>
+    <h1 v-if="showTitle" class="author__site-title">
+      {{ $static.metadata.siteName }}
+    </h1>
+
+    <p class="author__intro">
+      Love to 📜 analyse, 📐 design, 🛠️ build.
+    </p>
+
+    <p class="author__links">
+      <a href="https://twitter.com/ysajid0">Follow on Twitter</a>
+      <a href="https://github.com/ysajid">GitHub</a>
+    </p>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  props: ["showTitle"],
+};
+</script>
+
+<style lang="scss">
+.author {
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+  padding: calc(var(--space) / 2) 0;
+
+  &__image {
+    border-radius: 100%;
+    width: 90px;
+    height: 90px;
+    margin-bottom: 1em;
+  }
+
+  &__intro {
+    opacity: 0.8;
+  }
+
+  &__site-title {
+    font-size: 1.5em;
+  }
+
+  &__links {
+    margin-top: -0.5em;
+    a {
+      margin: 0 0.5em;
+    }
+  }
+}
+</style>
